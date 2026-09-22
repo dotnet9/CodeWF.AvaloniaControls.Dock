4487ebc20252ab1bc679f5b983811cdd8c5f83f5
Dotnet9(dotnet9.com) <1012434131@qq.com>
2026-09-22 18:48:39 +0800

    refactor: deprecate unused padding converter

--- src/CodeWF.AvaloniaControls.Dock/Converters/TrapezoidShapedDocumentTabStripItemPaddingConverter.cs
+++ src/CodeWF.AvaloniaControls.Dock/Converters/TrapezoidShapedDocumentTabStripItemPaddingConverter.cs
@@ -6,15 +6,15 @@ using System.Globalization;
 
 namespace CodeWF.AvaloniaControls.Dock.Converters;
 
+[Obsolete("The built-in themes do not use this converter. Use a dedicated application converter instead.")]
 public class TrapezoidShapedDocumentTabStripItemPaddingConverter : IValueConverter
 {
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (value is not DocumentTabStripItem { Parent: DocumentTabStrip tabControl } tabItem)
+        if (value is not DocumentTabStripItem tabItem)
         {
             return new Thickness(0);
         }
-
 
         var padding = tabItem.Padding;
         return new Thickness(padding.Left * 2, padding.Top, padding.Right * 2, padding.Bottom);
@@ -22,6 +22,6 @@ public class TrapezoidShapedDocumentTabStripItemPaddingConverter : IValueConvert
 
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        throw new NotImplementedException();
+        throw new NotSupportedException("This converter only supports one-way conversion.");
     }
 }
